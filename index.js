7f85270cbe560eb7e8d304617c9c55123427fb8b
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-04-12 12:39:39 +0200

    Challenge 6 html css js responsive API advice

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const adviceMessage = document.querySelector(".advice-random");
 const id = document.querySelector(".advice-number");
 
 
-
-
 btnAdvice.addEventListener('click', () => {
     const url = 'https://api.adviceslip.com/advice';
-
+    
     fetch(url).then((response)=>
     response.json().then((data)=> 
     
     {
-        adviceMessage.innerHTML =  `${data.slip.advice}`
+        adviceMessage.innerHTML =  ` "${data.slip.advice}"`
         id.innerHTML = `Advice #${data.slip.id}`
+        adviceMessage.style.animation = "fade 2s";
     }
-
+    
     ));
-
+    
 })
